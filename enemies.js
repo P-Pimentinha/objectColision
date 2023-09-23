43b7c36d967107660832cb0896702a1efd156bb6
Pedro Pimentinha <pedro.pimentinha18@gmail.com>
2023-09-23 18:09:10 +0200

    deletes: obstacles.js // enemies.js // index.js

--- enemies.js
+++
@@ -1,8 +0,0 @@
-function drawEnemies(ctx, arr) {
-  arr.forEach((obstacle) => {
-    ctx.fillStyle = 'yellow';
-    ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
-  });
-}
-
-export { drawEnemies };
